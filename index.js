b8e15ff5be83b083f8c3faf66903573ac9b8b6f4
dawoon-joo <wnekdns5015@gmail.com>
2023-05-24 10:58:30 +0900

    slide class 작업

--- index.js
+++ index.js
@@ -1,55 +1,70 @@
-const slides = document.querySelector('.slides'); 
-let slideImg = document.querySelectorAll('.slides li'); 
-const slideCount = slideImg.length; 
-const slideWidth = slides.offsetWidth;
-const prev = document.querySelector('.prev'); 
-const next = document.querySelector('.next'); 
-const count = 0;
+class SlideTest {
+  constructor(target){
+    this.init(target);
+  }
+  init(target){
+    this.container = document.querySelector(target);
+    this.slides = this.container.querySelector('#slides'); 
+    this.slideImg = this.slides.querySelectorAll('.slides li'); 
+    this.slideCount = this.slideImg.length; 
+    this.slideWidth = this.slides.offsetWidth;
+    this.prev = this.container.querySelector('.prev'); 
+    this.next = this.container.querySelector('.next'); 
+    this.count = 0;
+    this.enableClick = true;
 
-//전체 슬라이드 컨테이너 넓이 설정
-slides.style.width = (slideWidth) * slideCount + 'px';
-//초기 상태
-slides.style.left = '-100%';
-slides.prepend(slides.lastElementChild);
+    //전체 슬라이드 컨테이너 넓이 설정
+    this.slides.style.width = (this.slideWidth) * this.slideCount + 'px';
+    //초기 상태
+    this.slides.style.left = '-100%';
+    this.slides.prepend(this.slides.lastElementChild);
 
-//2번째 슬라이드에 클래스 추가
-function updateSlideImages() {
-  slideImg = document.querySelectorAll('.slides li');
-  [...slideImg].map((slide ,i)=> {
-  i === 1 ? slide.classList.add('on') : slide.classList.remove('on');
-  });
-  // console.log(slideImg);
+    this.prev.addEventListener('click', () => {
+      if(this.enableClick == true){
+        this.prevSlide(this.count - 1);
+      }
+    });
+    
+    this.next.addEventListener('click', () => {
+      if(this.enableClick == true){
+        this.nextSlide(this.count + 1);
+      }
+    });
+    
+  }
+
+  updateSlideImages() {
+    this.slideImg = document.querySelectorAll('.slides li');
+    [...this.slideImg].map((slide ,i)=> {
+    i === 1 ? slide.classList.add('on') : slide.classList.remove('on');
+    });
+  }
+
+  nextSlide() {
+    this.slides.style.transition = 'left 1s';
+    this.slides.style.left = '-200%';
+    this.enableClick = false;
+    setTimeout(()=> {
+      this.slides.append(this.slides.firstElementChild);
+      this.slides.style.transition = 'none';
+      this.slides.style.left = '-100%';
+      this.updateSlideImages();
+      this.enableClick = true;
+    }, 1000);
+  }
+  
+  prevSlide() {
+    this.slides.style.transition = 'left 1s';
+    this.slides.style.left = '0';
+    this.enableClick = false;
+    setTimeout(()=>{
+      this.slides.prepend(this.slides.lastElementChild);
+      this.slides.style.transition = 'none';
+      this.slides.style.left = '-100%';
+      this.updateSlideImages();
+      this.enableClick = true;
+    }, 1000)
+  }
+  
+
 }
-
-function nextSlide() {
-  slides.style.transition = 'left 0.5s';
-  slides.style.left = '-200%';
-  setTimeout(function() {
-    slides.append(slides.firstElementChild);
-    slides.style.transition = 'none';
-    slides.style.left = '-100%';
-    updateSlideImages()
-  }, 500);
-}
-
-function prevSlide() {
-  slides.style.transition = 'left 0.5s';
-  slides.style.left = '0';
-  setTimeout(function(){
-    slides.prepend(slides.lastElementChild);
-    slides.style.transition = 'none';
-    slides.style.left = '-100%';
-    updateSlideImages()
-  }, 500)
-}
-
-
-prev.addEventListener('click', function () {
-  prevSlide(count - 1);
-});
-
-next.addEventListener('click', function () {
-  nextSlide(count + 1);
-});
-
-
