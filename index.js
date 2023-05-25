99ee0d6429c8846c0bc69ef81ae47b928e71acf9
dawoon-joo <wnekdns5015@gmail.com>
2023-05-25 10:52:13 +0900

    tabbtn

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ class SlideTest {
     this.enableClick = true;
     this.startPoint = 0;
     this.endPoint = 0;
+    this.tabs = this.container.querySelector('.tabs');
+    this.tabbtns = this.tabs.querySelectorAll('.tab');
+    this.tabbtn = this.tabs.querySelector('.tab');
 
     //전체 슬라이드 컨테이너 넓이 설정
     this.slides.style.width = `${100 * this.slideCount}%`;
@@ -31,6 +34,40 @@ class SlideTest {
         this.nextSlide(this.count + 1);
       }
     });
+
+    this.tabbtns.forEach((el, index) => {
+      el.addEventListener('click', () => {
+        for(let tab of this.tabbtns){
+          tab.classList.remove('on');
+        }
+        this.tabbtns[index].classList.add('on');
+
+        const selectedDataR = el.getAttribute('data-r');
+        let slideIndex = 0;
+    
+        this.slideImg.forEach((slide, index) => {
+          if (slide.getAttribute('data-r') === selectedDataR) {
+            slideIndex = index;
+            return;
+          }
+        });
+        
+        const slicedArray = Array.from(this.slideImg).slice(slideIndex - 1);
+        const movedElements = Array.from(this.slideImg).slice(0, (slideIndex - 1));
+        const rearrangedSlideImg = slicedArray.concat(movedElements);
+
+        this.enableClick = false;
+        this.slides.innerHTML = '';
+        rearrangedSlideImg.forEach(slide => {
+          this.slides.appendChild(slide);
+        });
+        
+        setTimeout(() => {
+          this.updateSlideImages();
+          this.enableClick = true;
+        }, 500);
+      })
+    })
     
     //pc touch slide
     this.slides.addEventListener('mousedown', (e) => {
@@ -56,11 +93,15 @@ class SlideTest {
   }
 
   updateSlideImages() {
-    this.slideImg = document.querySelectorAll('.slides li');
-    [...this.slideImg].map((slide ,i)=> {
-    i === 1 ? slide.classList.add('on') : slide.classList.remove('on');
+    this.slideImg = this.slides.querySelectorAll('.slides li');
+    this.slideImg.forEach((slide, index) => {
+      slide.classList.remove('on');
+      if (index === 1) {
+        slide.classList.add('on');
+      }
     });
   }
+  
 
   nextSlide() {
     this.slides.style.transition = 'left 1s';
